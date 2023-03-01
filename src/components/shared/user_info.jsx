157b7c8c88813cde077d0cd209e1d7f5a5b2feea
DAKOTA WONG <d82wong@uwaterloo.ca>
2023-03-01 11:45:35 -0500

    made userinfo panel consistent with viewrecord

--- src/components/shared/user_info.jsx
+++ src/components/shared/user_info.jsx
@@ -8,7 +8,7 @@ export default function doctorInfo({ doctorInfo = [] }) {
           <ul className='user-info-list'>
             {doctorInfo.map((doctorInfo) => {
               return (
-                <li className='user-info-list-item' key={doctorInfo.label}><label className='user-info-label' id={doctorInfo.label_id}>{doctorInfo.label}</label><span className='user-info-span' id={doctorInfo.value_id}>{doctorInfo.value}</span></li>
+                <li className='user-info-list-item rounded-md' key={doctorInfo.label}><label className='user-info-label' id={doctorInfo.label_id}>{doctorInfo.label}</label><span className='user-info-span' id={doctorInfo.value_id}>{doctorInfo.value}</span></li>
               ) 
             })}
           </ul>
