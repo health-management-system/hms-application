1ff20ac7a6fd4d593c237475fb9dcb5d9fb450fa
DAKOTA WONG <d82wong@uwaterloo.ca>
2023-03-03 13:24:22 -0500

    Added error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,16 +1,25 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, useNavigate } from "react-router-dom";
+import './ErrorPage.css'
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
+  let navigate = useNavigate()
+
+  const returnHome = () => {
+    navigate('/')
+  }
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        {/* <i>{error.statusText as string || error!.message! as string}</i> */}
-      </p>
+    <div className="error-background">
+      <div className="error-page">
+        <h1 className="page-not-found-header rounded-md">Page Not Found</h1>
+        <div className="error-div rounded-md">
+          The page you were looking for was not found. Please try going back to a previous page.
+        </div>
+        <button className="error-page-button hover:bg-priHover rounded-md" onClick={returnHome}>Return Home</button>
+      </div> 
     </div>
+
   );
 }
